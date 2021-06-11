99ef46444430be48e7a8de17805b9b36aa29b2bd
Lúdio <ludio.ao@gmail.com>
2021-06-10 21:50:08 -0300

    fix: corrige chamada ao cliente

--- src/Services/Client.php
+++ src/Services/Client.php
@@ -7,42 +7,46 @@ use GuzzleHttp\Client as GuzzleClient;
 abstract class Client
 {
     protected $client;
-    
+
     protected const ENV_PRODUCTION = 'https://secure.d4sign.com.br/api/v1/';
-    
+
     protected const ENV_SANDBOX = 'http://demo.d4sign.com.br/api/v1/';
-    
+
     public function __construct()
     {
         $baseUri = config('d4sign.mode') === 'production' ? self::ENV_PRODUCTION : self::ENV_SANDBOX;
-    
+
         $this->client = new GuzzleClient(
             [
                 'base_uri' => $baseUri,
-                'headers' => [
-                    'Accept' => 'application/json',
+                'headers'  => [
+                    'Accept'   => 'application/json',
                     'tokenAPI' => config('d4sign.token_api'),
                     'cryptKey' => config('d4sign.crypt_key')
                 ]
             ]
         );
     }
-    
+
     public function get(string $url, array $query = [])
     {
         return $this->client->get($url, $query);
     }
-    
+
     public function post(string $url, array $data = [])
     {
-        return $this->client->post($url, $data);
+        return $this->client->post($url, [
+            'json' => $data
+        ]);
     }
-    
+
     public function put(string $url, array $data)
     {
-        return $this->client->put($url, $data);
+        return $this->client->put($url, [
+            'json' => $data
+        ]);
     }
-    
+
     public function delete(string $url, array $data)
     {
         return $this->client->delete($url, $data);
